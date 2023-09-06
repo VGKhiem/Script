c80542a0d9197b3e8fd332ea44c6b064acca0525
Cac <106011430+VGKhiem@users.noreply.github.com>
2023-09-06 19:15:42 +0700

    Update PlayerId.lua

--- PlayerId.lua
+++ PlayerId.lua
@@ -1,5 +1,5 @@
 userids = {
-   [3518811545] = true, -- this is my userid, remove it and add as much as you want in it.
+   [1956860232] = true, -- this is my userid, remove it and add as much as you want in it.
    --[USERID HERE] = true,
    --[USERIDHERE] = true
 }
